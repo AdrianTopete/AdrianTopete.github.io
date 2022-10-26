186b2fba10655f9a3975d6309abdc482ab142ed9
AdrianTopete <adriant.7236@gmail.com>
2022-10-26 23:39:55 +0000

    saving sorting exercise

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -42,15 +42,15 @@ async function quickSort(array, left, right){
 async function partition(array, left, right){
     var pivot = array[Math.floor((right + left)/2)].value;
     while(left < right){
-		while(array[left].value <= pivot){ 
-            array[left]++;
+		while(array[left].value < pivot){ 
+            left++;
         }
-        while (array[right].value >= pivot){ 
-            array[right]--;
+        while (array[right].value > pivot){ 
+            right--;
         }
     }
         if(left < right){
-	    swap(array, array[left], array[right]);
+	    swap(array, left, right);
         updateCounter(quickCounter);
         await sleep();
 }
