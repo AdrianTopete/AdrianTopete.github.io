28ec76c862a9b003c7aa9d1bcd1c0d79e979be18
AdrianTopete <adriant.7236@gmail.com>
2023-04-26 01:03:02 +0000

    Finished Collision Project

--- video-game-course/collision/space.js
+++ video-game-course/collision/space.js
@@ -1,4 +1,4 @@
-(function(window, opspark, racket) {
+(function(window, opspark, racket, HelenKellersSecrets) {
   /**
    * Creates and returns the space module. Listens for SPAWN 
    * events, adding any bodies in the event
@@ -48,28 +48,40 @@
           hit: for (let j = i - 1; j > -1; j--) {
             const bodyB = active[j];
             
+            
             // TODO 1: Calculate hit test components
-            
-            
+            const distance = HelenKellersSecrets.phyz.getDistance(bodyA, bodyB);
+            const minimumDistance = bodyA.radius + bodyB.radius
               
             // TODO 2: Do collision check: how do we know if bodies are colliding?
-            if(/* replace with collision check */ false) {
-              // console.log('hit!');
+            if(distance <= minimumDistance) {
+             
               
               // TODO 3: Calculate springToX and springToY 
+              const angle = Math.atan2(bodyB.y - bodyA.y, bodyB.x - bodyA.x); 
+              const springToX = bodyA.x + (Math.cos(angle) * minimumDistance);
+            
+
+              const springToY = bodyA.y + (Math.sin(angle) * minimumDistance);
               
+
               
                 
               // TODO 4: Calculate acceleration to spring-to point, factor in dampeningForce
+              accelerationOnX = dampeningForce * (springToX - bodyB.x)
+              accelerationOnY = dampeningForce * (springToY - bodyB.y)
               
-              
+
               
               // TODO 5: Apply acceleration to bodyB
-              
+              bodyB.velocityX += accelerationOnX;
+              bodyB.velocityY += accelerationOnY;
               
               
               // TODO 6: Apply inverse acceleration to bodyA
-              
+              bodyA.velocityX -= accelerationOnX;
+              bodyA.velocityY -= accelerationOnY;
+
               
               
             }
@@ -78,4 +90,4 @@
       }
     };
   };
-}(window, window.opspark, window.opspark.racket));
+}(window, window.opspark, window.opspark.racket, window.HelenKellersSecrets));
