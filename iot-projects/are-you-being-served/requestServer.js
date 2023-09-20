5cf0d44fdccf3b14feec20829f3191df541a2f10
Adrian Topete <adriant.7236@gmail.com>
2023-09-19 20:00:03 -0500

    saving are-you-being-served again

--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,1 +1,17 @@
 // requestServer.js file
+const http = require("http");
+const request = require("request");
+const port = 8686; 
+
+http.createServer(function(req,res){
+request ('https://adriantopete.github.io/', function(error, response, body){
+    if (!body || !response || (error === null && response.statusCode !== 200)){
+        res.end("bad URL\n");
+        return;
+    }
+    if (response.statusCode === 200 && !error === true){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write(body); 
+    }
+})
+}).listen(port);
