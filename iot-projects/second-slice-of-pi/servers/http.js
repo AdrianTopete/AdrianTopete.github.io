1f7c4c3e38beb818e33623dff846f7afcbdf6946
AdrianTopete <adriant.7236@gmail.com>
2023-12-19 18:44:41 -0600

    finished second slice of pi

--- iot-projects/second-slice-of-pi/servers/http.js
+++ iot-projects/second-slice-of-pi/servers/http.js
@@ -1,11 +1,14 @@
 
 const express = require('express'),
-	cors = require('cors');
+	cors = require('cors'),
+	middleware = require('./../middleware/converter'),
+	bodyParser = require('body-parser');
 	
 var sensorRoutes = require('./../routes/sensors');
 var actuatorRoutes = require('./../routes/actuators');
 
 var app = express();
+app.use(bodyParser.json());
 app.use(cors()); 
 app.use('/pi/sensors', sensorRoutes);
 app.use('/pi/actuators', actuatorRoutes);
@@ -16,6 +19,6 @@ app.get('/pi', function(req, res){
    res.send('gateway accessed');
 });
 //I have looked through all files'
-
+app.use(middleware());
 module.exports = app;
 
